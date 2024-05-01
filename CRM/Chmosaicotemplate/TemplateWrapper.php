95a6e8f00fd83a4bb7813cf6a126b35eec316599
Celine Nicolas <celinen@canadahelps.org>
2024-05-01 13:46:49 -0400

    CRM-1974 Fix path to mosaico templates

--- CRM/Chmosaicotemplate/TemplateWrapper.php
+++ CRM/Chmosaicotemplate/TemplateWrapper.php
@@ -11,19 +11,19 @@ class CRM_Chmosaicotemplate_TemplateWrapper implements API_Wrapper {
       if (isset($res['metadata'])) {
         $metadata = json_decode($res['metadata'], TRUE);
         if ($res['title'] == 'Basic - Email With Gallery') {
-          $metadata['thumbnail'] = '/vendor/civicrm/canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Email%20With%20Gallery.jpg';
+          $metadata['thumbnail'] = '/vendor/civicrm/zz-canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Email%20With%20Gallery.jpg';
         }
         elseif ($res['title'] == 'Basic - Email No Gallery') {
-          $metadata['thumbnail'] = '/vendor/civicrm/canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Email%20No%20Gallery.jpg';
+          $metadata['thumbnail'] = '/vendor/civicrm/zz-canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Email%20No%20Gallery.jpg';
         }
         elseif ($res['title'] == 'Basic - Newsletter') {
-          $metadata['thumbnail'] = '/vendor/civicrm/canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Newsletter.jpg';
+          $metadata['thumbnail'] = '/vendor/civicrm/zz-canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Newsletter.jpg';
         }
         elseif ($res['title'] == 'Basic - Text Only') {
-          $metadata['thumbnail'] = '/vendor/civicrm/canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Text%20Only.jpg';
+          $metadata['thumbnail'] = '/vendor/civicrm/zz-canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Text%20Only.jpg';
         }
         elseif ($res['title'] == 'Basic - Thank You Email') {
-          $metadata['thumbnail'] = '/vendor/civicrm/canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Thank%20You%20Email.jpg';
+          $metadata['thumbnail'] = '/vendor/civicrm/zz-canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Thank%20You%20Email.jpg';
         }
       }
       $result['values'][$key]['metadata'] = json_encode($metadata);
