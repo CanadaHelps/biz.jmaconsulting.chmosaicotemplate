b64e80edbfebdc15b8c6a9672438d68780b3657e
Celine Nicolas <celinen@canadahelps.org>
2024-05-01 14:13:51 -0400

    CRM-1974 Fix path to mosaico templates

--- CRM/Chmosaicotemplate/Upgrader.php
+++ CRM/Chmosaicotemplate/Upgrader.php
@@ -217,66 +217,23 @@ class CRM_Chmosaicotemplate_Upgrader extends CRM_Chmosaicotemplate_Upgrader_Base
     return TRUE;
   }
 
-  /**
-   * Example: Run an external SQL script.
-   *
-   * @return TRUE on success
-   * @throws Exception
-  public function upgrade_4201() {
-    $this->ctx->log->info('Applying update 4201');
-    // this path is relative to the extension base dir
-    $this->executeSqlFile('sql/upgrade_4201.sql');
-    return TRUE;
-  } // */
 
 
-  /**
-   * Example: Run a slow upgrade process by breaking it up into smaller chunk.
-   *
-   * @return TRUE on success
-   * @throws Exception
-  public function upgrade_4202() {
-    $this->ctx->log->info('Planning update 4202'); // PEAR Log interface
+  ### BELOW THIS POINT: use new format. ### 
+  ### Example: upgrade_13001 => 1.3.x, upgrade function 001 ###
 
-    $this->addTask(E::ts('Process first step'), 'processPart1', $arg1, $arg2);
-    $this->addTask(E::ts('Process second step'), 'processPart2', $arg3, $arg4);
-    $this->addTask(E::ts('Process second step'), 'processPart3', $arg5);
-    return TRUE;
-  }
-  public function processPart1($arg1, $arg2) { sleep(10); return TRUE; }
-  public function processPart2($arg3, $arg4) { sleep(10); return TRUE; }
-  public function processPart3($arg5) { sleep(10); return TRUE; }
-  // */
-
-
-  /**
-   * Example: Run an upgrade with a query that touches many (potentially
-   * millions) of records by breaking it up into smaller chunks.
-   *
-   * @return TRUE on success
-   * @throws Exception
-  public function upgrade_4203() {
-    $this->ctx->log->info('Planning update 4203'); // PEAR Log interface
-
-    $minId = CRM_Core_DAO::singleValueQuery('SELECT coalesce(min(id),0) FROM civicrm_contribution');
-    $maxId = CRM_Core_DAO::singleValueQuery('SELECT coalesce(max(id),0) FROM civicrm_contribution');
-    for ($startId = $minId; $startId <= $maxId; $startId += self::BATCH_SIZE) {
-      $endId = $startId + self::BATCH_SIZE - 1;
-      $title = E::ts('Upgrade Batch (%1 => %2)', array(
-        1 => $startId,
-        2 => $endId,
-      ));
-      $sql = '
-        UPDATE civicrm_contribution SET foobar = whiz(wonky()+wanker)
-        WHERE id BETWEEN %1 and %2
-      ';
-      $params = array(
-        1 => array($startId, 'Integer'),
-        2 => array($endId, 'Integer'),
-      );
-      $this->addTask($title, 'executeSql', $sql, $params);
+  public function upgrade_13001() {
+    $this->ctx->log->info('Fix template paths');
+    $template = CRM_Core_DAO::executeQuery("SELECT metadata, id from `civicrm_mosaico_template`");
+    while ($template->fetch()) {
+      $metadata = json_decode($template->metadata, TRUE);
+      $metadata['template'] = str_replace('vendor/civicrm/canadahelps/', 'vendor/civicrm/zz-canadahelps/', $metadata['template']);
+      $metadata['thumbnail'] = str_replace('vendor/civicrm/canadahelps/', 'vendor/civicrm/zz-canadahelps/', $metadata['thumbnail']);
+      $updated_metavalue = json_encode($metadata);
+      CRM_Core_DAO::executeQuery(sprintf("UPDATE civicrm_mosaico_template SET metadata = '%s' WHERE id = %s ", $updated_metavalue, $template->id));
     }
     return TRUE;
-  } // */
+  }
+
 
 }
