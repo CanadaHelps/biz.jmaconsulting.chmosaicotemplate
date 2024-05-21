6bc71f52bedd8e40b8aa3a08aacdd1ad114c5283
Celine Nicolas <celinen@canadahelps.org>
2024-05-21 18:05:50 -0400

    CRM-2283 handle api3 and api4 when altering MosaicoTemplate.get results

--- CRM/Chmosaicotemplate/TemplateWrapper.php
+++ CRM/Chmosaicotemplate/TemplateWrapper.php
@@ -7,27 +7,34 @@ class CRM_Chmosaicotemplate_TemplateWrapper implements API_Wrapper {
   }
 
   public function toApiOutput($apiRequest, $result) {
-    foreach ($result['values'] as $key => $res) {
+    $loopResult = is_array($result) ? $result['values'] : $result; // hand both api3 and api4
+
+    foreach ($loopResult as $index => $template) {
       $metadata = [];
-      if (isset($res['metadata'])) {
-        $metadata = json_decode($res['metadata'], TRUE);
-        if ($res['title'] == 'Basic - Email With Gallery') {
+      if (isset($template['metadata'])) {
+        $metadata = json_decode($template['metadata'], TRUE);
+        if ($template['title'] == 'Basic - Email With Gallery') {
           $metadata['thumbnail'] = '/vendor/civicrm/zz-canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Email%20With%20Gallery.jpg';
         }
-        elseif ($res['title'] == 'Basic - Email No Gallery') {
+        elseif ($template['title'] == 'Basic - Email No Gallery') {
           $metadata['thumbnail'] = '/vendor/civicrm/zz-canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Email%20No%20Gallery.jpg';
         }
-        elseif ($res['title'] == 'Basic - Newsletter') {
+        elseif ($template['title'] == 'Basic - Newsletter') {
           $metadata['thumbnail'] = '/vendor/civicrm/zz-canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Newsletter.jpg';
         }
-        elseif ($res['title'] == 'Basic - Text Only') {
+        elseif ($template['title'] == 'Basic - Text Only') {
           $metadata['thumbnail'] = '/vendor/civicrm/zz-canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Text%20Only.jpg';
         }
-        elseif ($res['title'] == 'Basic - Thank You Email') {
+        elseif ($template['title'] == 'Basic - Thank You Email') {
           $metadata['thumbnail'] = '/vendor/civicrm/zz-canadahelps/biz.jmaconsulting.chmosaicotemplate/chtemplate/thumbnails/Basic%20-%20Thank%20You%20Email.jpg';
         }
       }
-      $result['values'][$key]['metadata'] = json_encode($metadata);
+      $loopResult[$index]['metadata'] = json_encode($metadata);
+    }
+    if ( is_array($result)  ) {
+      $result['values'] = $loopResult;
+    } else {
+      $result = $loopResult;
     }
     return $result;
   }
